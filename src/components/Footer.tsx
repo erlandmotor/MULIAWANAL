026f70dcfde5abe14a0f8d8b74f8ff645889818c
erlandmotor <155846904+erlandmotor@users.noreply.github.com>
2024-01-21 12:06:08 +0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,9 +10,9 @@ export default function Footer({}: Props) {
     <footer className='p-container flex flex-col gap-10 border-t-1 py-10 lg:py-14'>
       <div className=' flex flex-col justify-evenly gap-10 md:flex-row'>
         <div className='flex flex-col'>
-          <h2 className='text-primary-text text-xl'>Rian Septrianto Maulana, S.M</h2>
+          <h2 className='text-primary-text text-xl'>MULIAWAN AL - AMIEN, S.Ds., B.A</h2>
           <h3 className='text-primary-text text-medium font-light'>
-            Calon Anggota DPR-RI DAPIL X GRESIK - LAMONGAN
+            Calon Anggota DPRD KOTA DAPIL 1 SERANG KOTA
           </h3>
         </div>
         <div className='flex flex-col gap-3'>
@@ -35,15 +35,15 @@ export default function Footer({}: Props) {
         <div className='flex flex-col gap-3'>
           <h2 className='text-primary-text text-xl'>Information</h2>
           <div className='text-primary-text flex flex-col gap-1 font-light'>
-            <span>GRESIK - LAMONGAN</span>
-            <span>Jawa Timur - Indonesia</span>
-            <span>+62 813-3432-4541</span>
-            <span>riand.maulana@yahoo.com</span>
+            <span>KOTA SERANG</span>
+            <span>Jawa Barat - Indonesia</span>
+            <span>+62 877-3360-5621</span>
+            <span>muliawanal@gmail.com</span>
           </div>
         </div>
       </div>
       <p className='border-t-1 pt-10 text-center text-sm'>
-        © 2024 K1IT. All rights reserved
+        © 2024 MULIAWAN AL. All rights reserved
       </p>
     </footer>
   );
